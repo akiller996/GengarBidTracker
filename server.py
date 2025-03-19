8c303f93853d78c3104fc5001af73a819cdc2edd
akiller996 <alessandro.pontone96@libero.it>
2025-03-19 18:20:32 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 from flask import Flask
 import threading
-import asyncio
-import bot  # Importa il bot per mantenerlo attivo
+import bot
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return "GengarBidTracker è attivo!"
 
-# Avvia il bot in un thread separato
 def run_bot():
-    asyncio.run(bot.main())  # Esegui la funzione asincrona correttamente
+    bot.main()
 
-threading.Thread(target=run_bot, daemon=True).start()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    threading.Thread(target=run_bot, daemon=True).start()
+    app.run(host='0.0.0.0', port=5000)
