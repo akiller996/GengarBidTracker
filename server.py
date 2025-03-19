63609f0152d0ce88b1501e27b139760035433236
akiller996 <alessandro.pontone96@libero.it>
2025-03-19 17:11:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 def home():
     return "GengarBidTracker è attivo!"
 
-# Avvia il bot in un thread separato
+# Funzione per avviare il bot in un thread separato
 def run_bot():
-    asyncio.run(bot.main())  # Esegui la funzione asincrona correttamente
+    loop = asyncio.new_event_loop()  # Crea un nuovo event loop
+    asyncio.set_event_loop(loop)  # Imposta il nuovo loop nel thread
+    loop.run_until_complete(bot.main())  # Avvia il bot Telegram
 
+# Avvia il bot in un thread separato come daemon
 threading.Thread(target=run_bot, daemon=True).start()
 
 if __name__ == "__main__":
